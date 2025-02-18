cf117001b3a0305add0c5beff912669741cb0c72
Kabir Deula <kabirdeula167@gmail.com>
2025-02-18 14:13:23 +0545

    feat: ✨Add OnboardingCubit for page navigation and update dependencies

---
+++ lib/features/onboarding/cubit/onboarding_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class OnboardingCubit extends Cubit<int> {
+  final PageController pageController = PageController();
+
+  OnboardingCubit() : super(0);
+
+  void nextPage({required int totalPages}) {
+    if (state < totalPages - 1) {
+      pageController.nextPage(
+        duration: Duration(milliseconds: 500),
+        curve: Curves.ease,
+      );
+    }
+  }
+
+  void updatePage({required int index}) => emit(index);
+}
