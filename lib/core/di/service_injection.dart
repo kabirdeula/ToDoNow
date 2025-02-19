c983a1c306c8f7488c7d4ca51f55cc18dcbe62df
Kabir Deula <kabirdeula167@gmail.com>
2025-02-19 13:52:12 +0545

    feat: ✨Setup service injection for AuthService registration

--- lib/core/di/service_injection.dart
+++ lib/core/di/service_injection.dart
@@ -1,1 +1,7 @@
-void setupServiceInjection() {}
+import 'package:to_do_now/features/authentication/authentication.dart';
+
+import 'di.dart';
+
+void setupServiceInjection() {
+  sl.registerSingleton<AuthService>(AuthService());
+}
