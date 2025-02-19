325d8bcb2dc24b24fe255eee21dbb844b547cf85
Kabir Deula <kabirdeula167@gmail.com>
2025-02-19 13:16:35 +0545

    feat: ✨Initialize Firebase in main function for backend integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:to_do_now/firebase_options.dart';
 
 import 'core/di/di.dart';
 import 'core/themes/themes.dart';
@@ -7,8 +9,10 @@ import 'features/authentication/authentication.dart';
 import 'features/onboarding/onboarding.dart';
 import 'routes/routes.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   setupDependencyInjection();
 
