3af9c3b2e0de15646b329c18ca7f42e445e148c6
Kabir Deula <kabirdeula167@gmail.com>
2025-02-19 13:40:52 +0545

    feat: ✨Add AuthResponse model for authentication response handling

--- lib/features/authentication/authentication.dart
+++ lib/features/authentication/authentication.dart
@@ -1,3 +1,4 @@
 export './cubit/auth_cubit.dart';
+export './data/models/auth_response/auth_response.dart';
 export './data/models/user/user.dart';
 export './presentation/screens/login_screen.dart';
---
+++ lib/features/authentication/data/models/auth_response/auth_response.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$AuthResponseImpl _$$AuthResponseImplFromJson(Map<String, dynamic> json) =>
+    _$AuthResponseImpl(
+      success: json['success'] as String?,
+      error: json['error'] as String?,
+    );
+
+Map<String, dynamic> _$$AuthResponseImplToJson(_$AuthResponseImpl instance) =>
+    <String, dynamic>{
+      'success': instance.success,
+      'error': instance.error,
+    };
