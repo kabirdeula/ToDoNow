51f2ccadcf46664ca47faf35e99e775f2ce85b7c
Kabir Deula <kabirdeula167@gmail.com>
2025-02-19 10:34:38 +0545

    feat: ✨Implement LoginScreen layout with form fields and buttons

--- lib/features/authentication/presentation/screens/login_screen.dart
+++ lib/features/authentication/presentation/screens/login_screen.dart
@@ -1,10 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_now/core/widgets/buttons/custom_elevated_button.dart';
+import 'package:to_do_now/routes/routes.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => context.pop(),
+          icon: Icon(Icons.arrow_back_ios_new),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 16,
+          children: [
+            Text('Login'),
+            Text('Username'),
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter your Username',
+              ),
+            ),
+            Text('Password'),
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: '********',
+              ),
+            ),
+            const Spacer(),
+            SizedBox(
+              width: double.infinity,
+              child: CustomElevatedButton.filled(
+                label: 'login',
+                onPressed: () {},
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(child: Divider()),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('OR'),
+                ),
+                Expanded(child: Divider()),
+              ],
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: CustomElevatedButton.outline(
+                label: 'login with google',
+                onPressed: () {},
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: CustomElevatedButton.outline(
+                label: 'login with google',
+                onPressed: () {},
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Text("Don't have an account? Register"),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
