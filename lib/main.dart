4dacfcd3a8efa462aeef82e6c3d066015a4c8c4f
Kabir Deula <kabirdeula167@gmail.com>
2025-02-20 16:37:31 +0545

    feat: ✨Add Hive utility for managing Hive boxes and initialize Hive in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'core/di/di.dart';
 import 'core/themes/themes.dart';
@@ -12,6 +13,8 @@ import 'routes/routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
