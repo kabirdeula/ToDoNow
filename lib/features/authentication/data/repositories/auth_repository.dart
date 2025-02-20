e8a6361ae097a7786b83bb3361f9b77e792832a3
Kabir Deula <kabirdeula167@gmail.com>
2025-02-20 13:23:23 +0545

    feat: ✨Add register method to AuthRepository for user registration

--- lib/features/authentication/data/repositories/auth_repository.dart
+++ lib/features/authentication/data/repositories/auth_repository.dart
@@ -4,10 +4,14 @@ import 'package:to_do_now/features/authentication/authentication.dart';
 class AuthRepositoryImpl implements AuthRepository {
   @override
   Future<AuthResponse> login({required UserModel user}) async {
-    var result = await sl<AuthService>().login(user: user);
-    return result;
+    return await sl<AuthService>().login(user: user);
   }
 
   @override
   Future googleLogin() async => await sl<AuthService>().loginWithGoogle();
+
+  @override
+  Future<AuthResponse> register({required UserModel user}) async {
+    return await sl<AuthService>().register(user: user);
+  }
 }
