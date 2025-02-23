72f44b422b0982c74fb7eb2bf6d9cd5ed9277d5c
Kabir Deula <kabirdeula167@gmail.com>
2025-02-23 17:55:14 +0545

    feat: ✨Refactor TaskUsecase to utilize TaskRepository for task management operations

--- lib/features/task/domain/usecase/task_usecase.dart
+++ lib/features/task/domain/usecase/task_usecase.dart
@@ -2,11 +2,23 @@ import 'package:to_do_now/core/di/di.dart';
 import 'package:to_do_now/features/task/task.dart';
 
 class TaskUsecase {
+  final TaskRepository _repository = sl<TaskRepository>();
+
   Future<void> addTask({required TaskModel task}) async {
-    await sl<TaskRepository>().addTask(task);
+    await _repository.addTask(task);
   }
 
-  Future<List<TaskModel>> getTasks() async {
-    return await sl<TaskRepository>().getTasks();
+  Future<List<TaskModel>> getTasks() async => await _repository.getTasks();
+
+  Future<void> deleteTask({required TaskModel task}) async {
+    await _repository.deleteTask(task);
+  }
+
+  Future<void> deleteSelectedTasks(Set<String> selectedTaskIds) async {
+    await _repository.deleteSelectedTasks(selectedTaskIds);
+  }
+
+  Future<void> updateTask({required TaskModel task}) async {
+    await _repository.updateTask(task);
   }
 }
