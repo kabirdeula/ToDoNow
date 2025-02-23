19c6971d58dac78fbc74c8d15c7b00c142b33851
Kabir Deula <kabirdeula167@gmail.com>
2025-02-23 10:37:00 +0545

    feat: ✨Implement DashboardScreen layout with AppBar, FAB, and NavigationBar

--- lib/features/dashboard/presentation/screens/dashboard_screen.dart
+++ lib/features/dashboard/presentation/screens/dashboard_screen.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_now/core/constants/constants.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.menu),
+        title: Text('Hello'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: CircleAvatar(child: Icon(Icons.person)),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.hourglass_empty, size: 180),
+            Text('Tap + to add your tasks'),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppColors.primary,
+        shape: CircleBorder(),
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(
+              icon: Icon(Icons.calendar_month_outlined), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.access_time), label: 'Home'),
+          NavigationDestination(
+              icon: Icon(Icons.person_outline), label: 'Profile'),
+        ],
+      ),
+    );
   }
 }
