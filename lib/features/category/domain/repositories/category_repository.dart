9f307f7ea7761a2b3e66d3a53d882007c1de780f
Kabir Deula <kabirdeula167@gmail.com>
2025-03-17 16:36:18 +0545

    feat: ✨Implement category repository with local and remote services for category management

--- lib/features/category/domain/repositories/category_repository.dart
+++ lib/features/category/domain/repositories/category_repository.dart
@@ -1,7 +1,7 @@
 import '../../category.dart';
 
 abstract class CategoryRepository {
-  Future<void> addCategory(CategoryEntity category);
-  Future<void> deleteCategory(String categoryId);
-  List<CategoryEntity> getCategories();
+  Future<void> saveCategory(CategoryEntity category);
+  Future<void> deleteCategory(String id);
+  Future<List<CategoryEntity>> getCategories();
 }
